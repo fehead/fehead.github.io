b34ade0228799face1a2990eb9e8ec337d826540
fehead <im.fehead@gmail.com>
2021-07-11 15:27:06 +0900

    ARM64 head.S 분석 1  post 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -22,4 +22,10 @@ var store = [{
         "tags": [],
         "url": "/linux/kernel/iamroot/iamroot18-7th/",
         "teaser": null
+      },{
+        "title": "ARM64 head.S 분석 1",
+        "excerpt":"ARM64 linux 5.10 kernel 분석 head.S 분석 1 head.S 파일 들어가기 $ vim :ts head.S 76 /* 77 * Kernel startup entry point. 78 * --------------------------- 79 * 80 * The requirements are: 81 * MMU = off, D-cache = off, I-cache = on or off, 82 * x0 =...","categories": ["arm64","kernel"],
+        "tags": [],
+        "url": "/arm64/kernel/arm64-head.S/",
+        "teaser": null
       }]
